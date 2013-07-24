125fff306de5fdc132794f8d7793dc6f5f2e247b
Matt Hoffman <hoffmanm@cs.ubc.ca>
2013-07-24 14:46:08 +0100

    Add a "setup" script for osx that's barely useful.

---
+++ osx_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+COMPUTER_NAME="MattsBook-Pro"
+
+# setup the computer's name.
+sudo scutil --set ComputerName $COMPUTER_NAME
+sudo scutil --set HostName $COMPUTER_NAME
+sudo scutil --set LocalHostName $COMPUTER_NAME
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $COMPUTER_NAME
+
