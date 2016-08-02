df0cd5d8bb31bf2013d7543f1543e0ab1e40261e
Matt Hoffman <mwhoffman@google.com>
2016-08-02 18:53:55 +0100

    Minor update to dotfiles.

--- osx_setup.sh
+++ osx_setup.sh
@@ -8,3 +8,14 @@ sudo scutil --set HostName $COMPUTER_NAME
 sudo scutil --set LocalHostName $COMPUTER_NAME
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $COMPUTER_NAME
 
+defaults write com.apple.mail PreferPlainText -bool TRUE
+
+# install homebrew
+HOMEBREW='/usr/local/homebrew'
+sudo mkdir $HOMEBREW && sudo chown $USER $HOMEBREW
+curl -L https://github.com/Homebrew/homebrew/tarball/master | tar xz --strip 1 -C $HOMEBREW
+$HOMEBREW/bin/brew install coreutils
+
+# allow keys to be held
+defaults write -g ApplePressAndHoldEnabled -bool false
+
