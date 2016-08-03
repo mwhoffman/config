fb12ee2d5e3581ac6913a29ba490a69705ea7b75
Matt Hoffman <mwhoffman@google.com>
2016-08-03 09:34:40 +0100

    Grab nerdtree.

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 # install vim packages (including the simple pathogen manager)
 mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/colors
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
-git clone https://github.com/edkolev/tmuxline.vim ~/.vim/bundle/tmuxline.vim
+git clone https://github.com/edkolev/tmuxline.vim ~/.vim/bundle/tmuxline
 
