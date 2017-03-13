e027ea446fa5763246750b3a7453b0a35ccd1005
Matt Hoffman <mhoffman@gmail.com>
2017-03-13 23:47:59 +0000

    Update hammerspoon and tmux configs.

--- dotfiles/hammerspoon/init.lua
+++ dotfiles/hammerspoon/init.lua
@@ -1,6 +1,5 @@
 -- require my local layout
 local layout = require('layout')
-local bind = hs.hotkey.bind
 
 -- define the hyper keys
 local mash1 = {'ctrl', 'shift'}
@@ -16,39 +15,55 @@ hs.grid.MARGINX = 0
 hs.grid.MARGINY = 0
 hs.grid.ui.showExtraKeys = false
 
--- bind keys for changing layouts of the current window
-bind(mash1, '0', layout.set_layout('full'))
-bind(mash1, '1', layout.set_layout('nw'))
-bind(mash1, '2', layout.set_layout('sw'))
-bind(mash1, '3', layout.set_layout('ne'))
-bind(mash1, '4', layout.set_layout('se'))
-bind(mash1, '5', layout.set_layout('west'))
-bind(mash1, '6', layout.set_layout('east'))
+-- hs.hotkey.bind keys for changing layouts of the current window
+hs.hotkey.bind(mash1, '0', layout.set_layout('full'))
+hs.hotkey.bind(mash1, '1', layout.set_layout('nw'))
+hs.hotkey.bind(mash1, '2', layout.set_layout('sw'))
+hs.hotkey.bind(mash1, '3', layout.set_layout('ne'))
+hs.hotkey.bind(mash1, '4', layout.set_layout('se'))
+hs.hotkey.bind(mash1, '5', layout.set_layout('west'))
+hs.hotkey.bind(mash1, '6', layout.set_layout('east'))
 
--- change the focus
-bind(mash1, 'h', hs.window.filter.focusWest)
-bind(mash1, 'j', hs.window.filter.focusSouth)
-bind(mash1, 'k', hs.window.filter.focusNorth)
-bind(mash1, 'l', hs.window.filter.focusEast)
-
--- show a grid to resize windows
-bind(mash1, 'a', hs.grid.show)
-bind(mash1, 'r', hs.reload)
-bind(mash1, 'o', hs.openConsole)
-
-function activate(name)
-  return function()
-    local app = hs.application.find(name)
-    app:activate()
-  end
+function move_mouse_to_window()
+  local frame = hs.window.focusedWindow():frame()
+  local pt = hs.geometry.rectMidPoint(frame)
+  hs.mouse.setAbsolutePosition(pt)
 end
 
-bind(mash2, 'm', activate('Mail'))
-bind(mash2, 'e', activate('Messages'))
-bind(mash2, 's', activate('Slack'))
-bind(mash2, 't', activate('Things'))
-bind(mash2, 'n', activate('Alternote'))
-bind(mash2, 'i', activate('iTunes'))
+-- change the focus
+hs.hotkey.bind(mash1, 'h', function() hs.window.filter.focusWest(); move_mouse_to_window() end)
+hs.hotkey.bind(mash1, 'j', function() hs.window.filter.focusSouth(); move_mouse_to_window() end)
+hs.hotkey.bind(mash1, 'k', function() hs.window.filter.focusNorth(); move_mouse_to_window() end)
+hs.hotkey.bind(mash1, 'l', function() hs.window.filter.focusEast(); move_mouse_to_window() end)
+
+hs.hotkey.bind(mash1, "space", function ()
+  focusScreen(hs.window.focusedWindow():screen():next())
+end)
+
+--Predicate that checks if a window belongs to a screen
+function isInScreen(screen, win)
+  return win:screen() == screen
+end
+
+function focusScreen(screen)
+  --Get windows within screen, ordered from front to back.
+  --If no windows exist, bring focus to desktop. Otherwise, set focus on
+  --front-most application window.
+  local windows = hs.fnutils.filter(
+      hs.window.orderedWindows(),
+      hs.fnutils.partial(isInScreen, screen))
+  local windowToFocus = #windows > 0 and windows[1] or hs.window.desktop()
+  windowToFocus:focus()
+
+  -- Move mouse to center of screen
+  local pt = hs.geometry.rectMidPoint(screen:fullFrame())
+  hs.mouse.setAbsolutePosition(pt)
+end
+
+-- show a grid to resize windows
+hs.hotkey.bind(mash1, 'a', hs.grid.show)
+hs.hotkey.bind(mash1, 'r', hs.reload)
+hs.hotkey.bind(mash1, 'o', hs.openConsole)
 
 -- display to show we've reloaded
 hs.alert.show('Reloaded hammerspoon config')
