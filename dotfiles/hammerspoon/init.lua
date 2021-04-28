84bdaa345b7b2faac48001cd8ce569782c7aabe3
Matt Hoffman <mwhoffman@google.com>
2021-04-28 12:24:30 +0100

    Fix hammerspoon changes after an update.

--- dotfiles/hammerspoon/init.lua
+++ dotfiles/hammerspoon/init.lua
@@ -1,9 +1,20 @@
+local window = require("hs.window") -- make sure this is loaded
+local winMT  = getmetatable(window)
+local original_index = winMT.__index
+winMT.__index = function(self, key)
+    local answer = original_index(self, key)
+    if answer then
+        return answer
+    else
+        return hs.getObjectMetatable("hs.window")[key]
+    end
+end
+
 -- require my local layout
 local layout = require('layout')
 
 -- define the hyper keys
-local mash1 = {'ctrl', 'shift'}
-local mash2 = {'ctrl', 'shift'}
+local hyper = {'ctrl', 'shift'}
 
 -- don't use window animations
 hs.window.animationDuration = 0
@@ -16,13 +27,13 @@ hs.grid.MARGINY = 0
 hs.grid.ui.showExtraKeys = false
 
 -- hs.hotkey.bind keys for changing layouts of the current window
-hs.hotkey.bind(mash1, '0', layout.set_layout('full'))
-hs.hotkey.bind(mash1, '1', layout.set_layout('nw'))
-hs.hotkey.bind(mash1, '2', layout.set_layout('sw'))
-hs.hotkey.bind(mash1, '3', layout.set_layout('ne'))
-hs.hotkey.bind(mash1, '4', layout.set_layout('se'))
-hs.hotkey.bind(mash1, '5', layout.set_layout('west'))
-hs.hotkey.bind(mash1, '6', layout.set_layout('east'))
+hs.hotkey.bind(hyper, '0', layout.set_layout('full'))
+hs.hotkey.bind(hyper, '1', layout.set_layout('nw'))
+hs.hotkey.bind(hyper, '2', layout.set_layout('sw'))
+hs.hotkey.bind(hyper, '3', layout.set_layout('ne'))
+hs.hotkey.bind(hyper, '4', layout.set_layout('se'))
+hs.hotkey.bind(hyper, '5', layout.set_layout('west'))
+hs.hotkey.bind(hyper, '6', layout.set_layout('east'))
 
 function move_mouse_to_window()
   local frame = hs.window.focusedWindow():frame()
@@ -31,39 +42,15 @@ function move_mouse_to_window()
 end
 
 -- change the focus
-hs.hotkey.bind(mash1, 'h', function() hs.window.filter.focusWest(); move_mouse_to_window() end)
-hs.hotkey.bind(mash1, 'j', function() hs.window.filter.focusSouth(); move_mouse_to_window() end)
-hs.hotkey.bind(mash1, 'k', function() hs.window.filter.focusNorth(); move_mouse_to_window() end)
-hs.hotkey.bind(mash1, 'l', function() hs.window.filter.focusEast(); move_mouse_to_window() end)
-
-hs.hotkey.bind(mash1, "space", function ()
-  focusScreen(hs.window.focusedWindow():screen():next())
-end)
-
---Predicate that checks if a window belongs to a screen
-function isInScreen(screen, win)
-  return win:screen() == screen
-end
-
-function focusScreen(screen)
-  --Get windows within screen, ordered from front to back.
-  --If no windows exist, bring focus to desktop. Otherwise, set focus on
-  --front-most application window.
-  local windows = hs.fnutils.filter(
-      hs.window.orderedWindows(),
-      hs.fnutils.partial(isInScreen, screen))
-  local windowToFocus = #windows > 0 and windows[1] or hs.window.desktop()
-  windowToFocus:focus()
-
-  -- Move mouse to center of screen
-  local pt = hs.geometry.rectMidPoint(screen:fullFrame())
-  hs.mouse.setAbsolutePosition(pt)
-end
+hs.hotkey.bind(hyper, 'h', function() hs.window.filter.focusWest(); move_mouse_to_window() end)
+hs.hotkey.bind(hyper, 'j', function() hs.window.filter.focusSouth(); move_mouse_to_window() end)
+hs.hotkey.bind(hyper, 'k', function() hs.window.filter.focusNorth(); move_mouse_to_window() end)
+hs.hotkey.bind(hyper, 'l', function() hs.window.filter.focusEast(); move_mouse_to_window() end)
 
 -- show a grid to resize windows
-hs.hotkey.bind(mash1, 'a', hs.grid.show)
-hs.hotkey.bind(mash1, 'r', hs.reload)
-hs.hotkey.bind(mash1, 'o', hs.openConsole)
+hs.hotkey.bind(hyper, 'a', hs.grid.show)
+hs.hotkey.bind(hyper, 'r', hs.reload)
+hs.hotkey.bind(hyper, 'o', hs.openConsole)
 
 -- display to show we've reloaded
 hs.alert.show('Reloaded hammerspoon config')
