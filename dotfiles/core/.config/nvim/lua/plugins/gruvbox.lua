261071f82ec49dd78f28b9e69afa3ccabdd56193
Matt Hoffman <mhoffman@gmail.com>
2024-08-23 20:45:28 +0100

    Fix bg error in my neovim gruvbox config.

--- dotfiles/core/.config/nvim/lua/plugins/gruvbox.lua
+++ dotfiles/core/.config/nvim/lua/plugins/gruvbox.lua
@@ -4,11 +4,29 @@ return {
     name='gruvbox',
     priority=1000,
     config=function()
-      require('gruvbox').setup({
-        overrides = {
-          SignColumn = {bg='bg'},
-        },
-      })
+      -- Include the package and save the load function.
+      gruvbox = require('gruvbox')
+      gruvbox_load = gruvbox.load
+
+      -- Override the load function to check the background option.
+      gruvbox.load = function()
+        -- Set the background based on whether we want a dark theme or not.
+        if vim.o.background == 'dark' then
+          bg = gruvbox.palette.dark0
+        else
+          bg = gruvbox.palette.light0
+        end
+
+        -- Run the setup; override SignColumn's bg color.
+        gruvbox.setup({
+          overrides = {
+            SignColumn={bg=bg},
+          },
+        })
+
+        -- Run the original load.
+        return gruvbox_load()
+      end
     end
   }
 }
