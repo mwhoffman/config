3460951974a82266f1e6a502ab8cc1a672ac9bb2
Matt Hoffman <mhoffman@gmail.com>
2024-08-27 22:42:46 +0100

    Add telescope to nvim.

---
+++ dotfiles/core/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,19 @@
+local function extra_config ()
+  vim.keymap.set('n', '<leader>f', ':Telescope find_files<cr>', {silent=true})
+end
+
+return {
+  {
+    'nvim-telescope/telescope.nvim',
+    name='telescope',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'BurntSushi/ripgrep',
+    },
+    opts = function()
+      extra_config()
+
+      return {}
+    end,
+  }
+}
