3c5ba3ab34034a5d22b0d0ac3c53353f69d94764
Matt Hoffman <mhoffman@gmail.com>
2024-09-08 00:04:54 +0100

    nvim: clean up buffer keymaps.

--- dotfiles/core/.config/nvim/lua/plugins/barbar.lua
+++ dotfiles/core/.config/nvim/lua/plugins/barbar.lua
@@ -19,11 +19,11 @@ return {
   },
   lazy = false,
   keys = {
-    {'<leader>p', ':BufferPin<cr>', silent=true},
     {'<leader>q', ':BufferClose<cr>', silent=true},
-    {'<leader>Q', ':BufferCloseAllButCurrentOrPinned<cr>', silent=true},
+    {'<leader>Q', ':BufferCloseAllButCurrent<cr>', silent=true},
     {'<leader>,', ':BufferPrevious<cr>', silent=true},
     {'<leader>.', ':BufferNext<cr>', silent=true},
+    {'<leader>0', ':BufferLast<cr>', silent=true},
     {'<leader>1', ':BufferGoto 1<cr>', silent=true},
     {'<leader>2', ':BufferGoto 2<cr>', silent=true},
     {'<leader>3', ':BufferGoto 3<cr>', silent=true},
@@ -33,7 +33,6 @@ return {
     {'<leader>7', ':BufferGoto 7<cr>', silent=true},
     {'<leader>8', ':BufferGoto 8<cr>', silent=true},
     {'<leader>9', ':BufferGoto 9<cr>', silent=true},
-    {'<leader>0', ':BufferLast<cr>', silent=true},
   },
   init = function()
     vim.g.barbar_auto_setup = false
