dc0f8ffe6aae8e9cca3521bb6105f7960eee8290
Matt Hoffman <mhoffman@gmail.com>
2024-09-19 00:37:21 +0100

    nvim: move lazy setup.

--- dotfiles/core/.config/nvim/init.lua
+++ dotfiles/core/.config/nvim/init.lua
@@ -1,28 +1,6 @@
 require("options")
 require("keymaps")
-
--- Define where to find the lazy plugin manager.
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
--- If lazy doesn't exist get it from github.
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({
-    "git", "clone", "--filter=blob:none", "--branch=stable",
-    lazyrepo, lazypath
-  })
-  if vim.v.shell_error ~= 0 then
-    error("Error cloning lazy.nvim:\n" .. out)
-  end
-end
-
--- Run lazy using plugins configured in ./lua/plugins.
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({
-  spec = "plugins",
-  change_detection = {enabled=false},
-  rocks = {enabled=false},
-})
+require("lazy-setup")
 
 -- Set the colorscheme.
 vim.cmd.colorscheme "gruvbox"
