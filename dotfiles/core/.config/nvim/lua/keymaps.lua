6a61709752f69846af9b42c7c6fa800385a82924
Matt Hoffman <mhoffman@gmail.com>
2024-09-25 01:31:03 +0100

    nvim: add a keymap for lsp hover.

--- dotfiles/core/.config/nvim/lua/keymaps.lua
+++ dotfiles/core/.config/nvim/lua/keymaps.lua
@@ -8,6 +8,7 @@ vim.keymap.set({"n", "v"}, "<c-j>", "<c-d>", {desc="Scroll downwards"})
 
 vim.keymap.set("c", "<c-a>", "<Home>")
 vim.keymap.set("c", "<c-e>", "<End>")
+vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
 
 -- Delete some useless builtins.
 vim.keymap.del("n", "&")
