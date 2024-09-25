4eaf85ef9942a53942ad6e57e5ba54615a233c0c
Matt Hoffman <mhoffman@gmail.com>
2024-09-25 22:41:21 +0000

    nvim: add keymaps for buffer next/prev.

--- dotfiles/core/.config/nvim/lua/keymaps.lua
+++ dotfiles/core/.config/nvim/lua/keymaps.lua
@@ -8,7 +8,10 @@ vim.keymap.set({"n", "v"}, "<c-j>", "<c-d>", {desc="Scroll downwards"})
 
 vim.keymap.set("c", "<c-a>", "<Home>")
 vim.keymap.set("c", "<c-e>", "<End>")
+
 vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
+vim.keymap.set("n", "<leader>n", "<cmd>bnext<cr>", {desc="Buffer next"})
+vim.keymap.set("n", "<leader>p", "<cmd>bprev<cr>", {desc="Buffer prev"})
 
 -- Delete some useless builtins.
 vim.keymap.del("n", "&")
