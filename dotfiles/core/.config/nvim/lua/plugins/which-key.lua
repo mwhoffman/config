913520f11fe8b3c285ba8b8253a7648090c2e7e5
Matt Hoffman <mhoffman@gmail.com>
2024-09-29 21:30:32 +0100

    nvim: clean up which-key config.

--- dotfiles/core/.config/nvim/lua/plugins/which-key.lua
+++ dotfiles/core/.config/nvim/lua/plugins/which-key.lua
@@ -1,6 +1,6 @@
 return {
   "folke/which-key.nvim",
-  name = "which-key",
+  event = "VeryLazy",
   opts = {
     preset = "helix",
     icons = {mappings=false},
@@ -23,12 +23,14 @@ return {
       {"<leader>c", group="Copy…"},
       {"<leader>f", group="Find…"},
       {"<leader>h", group="Harpoon…"},
+      {"<leader>s", group="Split…"},
       {"h", hidden=true, mode="n"},
       {"j", hidden=true, mode="n"},
       {"k", hidden=true, mode="n"},
       {"l", hidden=true, mode="n"},
       {"Y", desc="Yank to End of Line"},
       {"D", desc="Delete to End of Line"},
+      {"&", desc="Repeat last substitution"},
       {"<C-L>", desc="Clear screen"},
       {"/", hidden=true, mode="o"},
       {"?", hidden=true, mode="o"},
