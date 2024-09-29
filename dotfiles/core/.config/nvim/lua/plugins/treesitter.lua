2e75eea794cd6f090824bc946c9239f795b197cc
Matt Hoffman <mhoffman@gmail.com>
2024-09-29 21:30:03 +0100

    nvim: update treesitter config.

--- dotfiles/core/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/core/.config/nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,24 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  name = "treesitter",
   main = "nvim-treesitter.configs",
   build = ":TSUpdate",
   opts = {
     ensure_installed = {
-      "bash", "c", "diff", "html", "lua", "luadoc", "markdown",
-      "markdown_inline", "python", "query", "vim", "vimdoc"},
+      "bash", "diff", "html", "lua", "luadoc", "markdown", "markdown_inline",
+      "python", "vim", "vimdoc"
+    },
     -- Autoinstall languages that are not installed
     auto_install = true,
     highlight = {
       enable = true,
-      -- Remove any languages that rely on vim"s regex highlighting system (e.g.
+      -- Remove any languages that rely on vim's regex highlighting system (e.g.
       -- ruby) for indentation.
-      additional_vim_regex_highlighting = {
-        "ruby",
-      },
+      additional_vim_regex_highlighting = {"ruby"},
     },
     indent = {
       enable = true,
       -- Ditto above.
-      disable = {
-        "ruby",
-      }
+      disable = {"ruby"}
     },
   },
 }
