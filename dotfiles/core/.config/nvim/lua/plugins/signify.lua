9b5c3d540538b63fb30532120138d9d28d176766
Matt Hoffman <mhoffman@gmail.com>
2024-10-03 12:22:37 +0000

    nvim: re-enable signify; make it play nice with gitsigns.

--- dotfiles/core/.config/nvim/lua/plugins/signify.lua
+++ dotfiles/core/.config/nvim/lua/plugins/signify.lua
@@ -1,11 +1,11 @@
 return {
   "mhinz/vim-signify",
-  enabled = false,
   config = function()
-    vim.g.signify_vcs_list = {"hg"}
-    vim.g.signify_sign_add = "▌"
-    vim.g.signify_sign_change = "▌"
-    vim.g.signify_sign_delete = "▁▁"
-    vim.g.signify_sign_delete_first_line = "▀▀"
+    vim.g.signify_skip = {vcs={deny={"git"}}}
+    vim.g.signify_sign_add = "┃"
+    vim.g.signify_sign_change = "┃"
+    vim.g.signify_sign_delete = "▁"
+    vim.g.signify_sign_delete_first_line = "▔"
+    vim.g.signify_sign_change_delete = "~"
   end
 }
