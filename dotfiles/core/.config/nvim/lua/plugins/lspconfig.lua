9939a17f741f14e0bd2b0d4ce20cc8eb508bf56c
Matt Hoffman <mhoffman@gmail.com>
2024-10-05 17:51:08 +0100

    nvim: add overrides to lspconfig.

--- dotfiles/core/.config/nvim/lua/plugins/lspconfig.lua
+++ dotfiles/core/.config/nvim/lua/plugins/lspconfig.lua
@@ -5,23 +5,26 @@ return {
     "williamboman/mason-lspconfig.nvim",
   },
   config = function()
+    -- This defines local overrides that will be applied only if and when the
+    -- LSP is set up by mason-lspconfig. Each item in overrides defines the
+    -- table that is passed to the associated lspconfig setup call.
+    local overrides = {
+      basedpyright = {
+        settings = {
+          basedpyright = {
+            typeCheckingMode = "standard",
+          }
+        }
+      },
+    }
+
     require("mason").setup()
     require("mason-lspconfig").setup()
     require("mason-lspconfig").setup_handlers {
       function(server_name)
-        require("lspconfig")[server_name].setup {}
+        settings = overrides[server_name] or {}
+        require("lspconfig")[server_name].setup(settings)
       end
     }
-
-    -- TODO: remove the setup below for now because I don't want basedpyright
-    -- installed everywhere.
-
-    -- require("lspconfig")["basedpyright"].setup({
-    --   settings = {
-    --     basedpyright = {
-    --       typeCheckingMode = "standard",
-    --     }
-    --   }
-    -- })
   end
 }
