5ce1c5ae620f17ffc23d1b9352e0efd7c3f9a5f0
Matt Hoffman <mhoffman@gmail.com>
2024-10-07 19:25:32 +0000

    nvim: fix keymaps for todo-comments.

--- dotfiles/core/.config/nvim/lua/plugins/todo-comments.lua
+++ dotfiles/core/.config/nvim/lua/plugins/todo-comments.lua
@@ -4,28 +4,36 @@ return {
     "nvim-lua/plenary.nvim",
   },
   event = {"BufReadPre", "BufNewFile"},
-  config = function()
-    local tc = require("todo-comments")
-    local km = vim.keymap
-
-    km.set("n", "]t", tc.jump_next, {desc="Next todo comment"})
-    km.set("n", "[t", tc.jump_prev, {desc="Previous todo comment"})
-    km.set("n", "<leader>ft", "<cmd>:TodoTelescope<cr>", {desc="Find todos"})
-
-    tc.setup({
-      highlight = {
-        -- vimgrep regex which matches whitespace followed by a todo comment of
-        -- the form "TODO:" or "TODO(string):". Note, this will have false
-        -- positives for todos that appear within a comment due the fact that it
-        -- is not possible to search only at the beginning of a comment.
-        pattern = [[\s+<((KEYWORDS)%(\(.+\))?):]],
-        keyword = "wide",
-      },
-      search = {
-        -- Define the pattern to use for :TodoTelescope. See the above for a
-        -- note on "false positives".
-        pattern = [[\s+\b(KEYWORDS)(\(.+\))?:]],
-      },
-    })
-  end
+  keys = {
+    {
+      "]t",
+      function() return require("todo-comments").jump_next() end,
+      desc = "Next todo comment",
+    },
+    {
+      "[t",
+      function() return require("todo-comments").jump_prev() end,
+      desc = "Previous todo comment",
+    },
+    {
+      "<leader>ft",
+      "<cmd>:TodoTelescope<cr>",
+      desc = "Find todo comments",
+    },
+  },
+  opts = {
+    highlight = {
+      -- vimgrep regex which matches whitespace followed by a todo comment of
+      -- the form "TODO:" or "TODO(string):". Note, this will have false
+      -- positives for todos that appear within a comment due the fact that it
+      -- is not possible to search only at the beginning of a comment.
+      pattern = [[\s+<((KEYWORDS)%(\(.+\))?):]],
+      keyword = "wide",
+    },
+    search = {
+      -- Define the pattern to use for :TodoTelescope. See the above for a
+      -- note on "false positives".
+      pattern = [[\s+\b(KEYWORDS)(\(.+\))?:]],
+    },
+  }
 }
