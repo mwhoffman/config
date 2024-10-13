5705900176524205816a9d8c6961984d86923ef2
Matt Hoffman <mhoffman@gmail.com>
2024-10-13 12:41:20 +0100

    zsh: look for brew in a non-standard position last.

--- dotfiles/mac/.config/zsh/homebrew.zsh
+++ dotfiles/mac/.config/zsh/homebrew.zsh
@@ -5,7 +5,8 @@ function homebrew_path { [ -x $1 ] && eval "$($1 shellenv)"; }
 # Try to find the brew command and use the above command to set the environment
 # variables.
 homebrew_path "/opt/homebrew/bin/brew" || \
-homebrew_path "/usr/local/bin/brew"
+homebrew_path "/usr/local/bin/brew"  || \
+homebrew_path "/usr/local/brew/bin/brew"
 
 # Inlcude paths for gnu coreutils.
 PATH="$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin:$PATH"
